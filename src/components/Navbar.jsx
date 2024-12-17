d63d9bae3b5bd48b01f2dc301006bd6da16c25cb
Ulyana <danilovichyliana@gmail.com>
2024-12-18 02:03:29 +0300

    fourth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Logo from '../assets/logo.png'; 
+import Logo from '../assets/Logo.png'; 
 
 const Navbar = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
